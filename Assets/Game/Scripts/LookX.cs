a4a9fc11ce65347ebfc862fc80319462d05eda81
m-kaique <123603849+m-kaique@users.noreply.github.com>
2023-02-01 19:34:30 -0300

    Mouse Look
    X axis and Y axis

--- Assets/Game/Scripts/LookX.cs
+++ Assets/Game/Scripts/LookX.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LookX : MonoBehaviour
 {
+    private float _sensitivity = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,20 @@ public class LookX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float _mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += _mouseX * _sensitivity;
+        transform.localEulerAngles = newRotation;
+
+
+        /*transform.localEulerAngles = new Vector3(
+            transform.localEulerAngles.x, 
+            transform.localEulerAngles.y + (_mouseX * _sensitivity), 
+            transform.localEulerAngles.z
+            );
+        */
+
+
     }
 }
